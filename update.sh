1d4dec6100752882cd6b5d117c8dd30cc315adf9
quiprr <quiprr@ametrine.dev>
2020-08-02 17:35:25 -0700

    Updated Packages files for new debs (via update.sh)

--- update.sh
+++ update.sh
@@ -22,7 +22,7 @@ rm Packages.gz
 rm Packages.bz2
 
 # Recreate Packages files
-dpkg-scanpackages debs > Packages
+dpkg-scanpackages --multiversion debs > Packages
 gzip -c9 Packages > Packages.gz
 bzip2 -c9 Packages > Packages.bz2
 
